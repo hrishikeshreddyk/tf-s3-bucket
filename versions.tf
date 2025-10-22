c0c0088ceb20df440f2cb74b80b35668287a7376
hrishikeshreddyk <105866953+hrishikeshreddyk@users.noreply.github.com>
2025-10-22 14:03:20 -0500

    Create versions.tf

---
+++ versions.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  required_version = ">= 1.5.0"
+}
+
+provider "aws" {
+  region = "us-east-1"   # Change this to your preferred AWS region
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_access_key
+
+}
