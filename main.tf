21a356f76bf940ff247403c56c82705d269a8777
hrishikeshreddyk <105866953+hrishikeshreddyk@users.noreply.github.com>
2025-10-22 13:58:20 -0500

    Configure AWS provider with access keys
    Added AWS access and secret keys as variables.

--- main.tf
+++ main.tf
@@ -11,6 +11,9 @@ terraform {
 
 provider "aws" {
   region = "us-east-1"   # Change this to your preferred AWS region
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_access_key
+
 }
 
 resource "aws_s3_bucket" "my_bucket" {
